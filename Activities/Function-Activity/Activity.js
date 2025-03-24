6ffbad2f7459b25c46b955b11ee6072b5a1ffc3b
Stevenacedillera <stevendillera@gmail.com>
2025-03-23 20:42:17 -0700

    Activities

---
+++ Activities/Function-Activity/Activity.js
@@ -0,0 +1,15 @@
+function whatShallWear(temp){
+    if (temp < 60) {
+        console.log("Wear a jacekt");
+    }
+else if (temp < 70){
+    console.log("Wear a Sweater");
+}
+
+else {(temp < 70)
+    console.log("Wear a T-Shirt");
+}
+}
+whatShallWear(60);
+whatShallWear(80);
+whatShallWear(50);
